f995135b36836c96a41f413dd0d2a42b5362368c
JérômeGlt <jerome.gallot39@orange.fr>
2021-11-19 17:07:24 +0100

    dotenv et rate limiter ok

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
 require('dotenv').config()
-// const db = require('db')
-
-// db.connect({
-//     host: process.env.DB_HOST,
-//     username: process.env.DB_USER,
-//     password: process.env.DB_PASS
-// })
 
 const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
 const cors = require('cors')
 const helmet = require('helmet')
-const apiLimiter = require('./apiLimiter')
 
 const sauceRoutes = require('./routes/sauces')
 const userRoutes = require('./routes/user')
 const likeRoutes = require('./routes/likes')
+const rateLimiter = require('./middleware/rate-limit')
 
-mongoose.connect('mongodb+srv://jeromegallot:gallot@cluster0.wd2vi.mongodb.net/Cluster0?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://' + process.env.DB_LOGIN + ':' + process.env.DB_PASSWORD + '@' +
+process.env.DB + '.wd2vi.mongodb.net/' + process.env.DB + '?retryWrites=true&w=majority',
     {useNewUrlParser: true,
     useUnifiedTopology: true })
     .then(() => console.log('Connexion à MongoDB réussie !'))
@@ -33,10 +27,9 @@ app.use(express.json())
 app.use('/images', express.static(path.join(__dirname, 'images')))
 
 app.use('/api/sauces', sauceRoutes)
-app.use('/api/auth', userRoutes)
+app.use('/api/auth', rateLimiter, userRoutes)
 app.use('/api/sauces', likeRoutes)
 
 app.use(helmet())
-app.use('/api/', apiLimiter)
 
 module.exports = app
